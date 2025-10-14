3883e51bf33b1ee92b805cbdd4b6eca0225c10fd
jarofsocks <stepankazuta@gmail.com>
2025-10-14 13:52:52 +1000

    исправил 4

--- labs/lab4/4.py
+++ labs/lab4/4.py
@@ -1,5 +1,5 @@
 ino = int(input())
-if int(str(ino)[-1]) % 2 == 0 and sum([int(i) for i in str(ino).split('')]) % 3 == 0:
+if int(str(ino)[-1]) % 2 == 0 and sum([int(i) for i in str(ino)]) % 3 == 0:
     print('Делится на "6"')
 else:
     print('Не делится на "6"')
