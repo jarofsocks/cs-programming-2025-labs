ea9fcd886af5ca50180b17bea0614607a8326801
Казюта Степан <jarofsocks@stud.vvsu.ru>
2025-11-11 12:00:56 +1000

    knl/kl

--- labs/lab4/4.py
+++ labs/lab4/4.py
@@ -1,5 +1,10 @@
+<<<<<<< Updated upstream
 ino = input()
 if int(ino[-1]) % 2 == 0 and sum([int(i) for i in ino]) % 3 == 0:
+=======
+ino = int(input())
+if int(str(ino)[-1]) % 2 == 0 and sum([int(i) for i in str(ino)]) % 3 == 0:   
+>>>>>>> Stashed changes
     print('Делится на "6"')
 else:
     print('Не делится на "6"')
