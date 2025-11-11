6d5c5cd6c2a7f34f6868fba1121116b0a681de4d
Казюта Степан <jarofsocks@stud.vvsu.ru>
2025-11-11 12:19:45 +1000

    hgfhfghfdhfg

--- labs/lab4/5.py
+++ labs/lab4/5.py
@@ -2,19 +2,30 @@ from pydoc import splitdoc
 import string
 pswrd = input('Введите пароль: ')
 err_output = 'Пароль ненадежный: отсутствуют '
+cnt = 0
 
-upprcase = False
 for i in pswrd:
     if i in string.ascii_uppercase:
-        upprcase = True
+        cnt += 1
+if cnt == 0:
+    err_output += ', Заглавные буквы'
+cnt = 0
 
-nmbrs = False
 for i in pswrd:
     if i in string.digits:
-        nmbrs = True
+        cnt += 1
+if cnt == 0:
+    err_output += ', Строчные буквы'
+cnt = 0
 
-splc = False
 for i in pswrd:
     if i in string.punctuation:
-        splc = True
+        cnt += 1
+if cnt == 0:    
+    err_output += ', Специальные знаки'
+        
 
+if len(err_output) == 31: 
+    print("Пароль надежный")
+else: 
+    print(err_output)
