e3e7db98ff625b5b55b3be46768d09e0d8e73e43
Казюта Степан <jarofsocks@stud.vvsu.ru>
2025-12-09 13:19:40 +1000

    made it!

--- labs/lab5/8.py
+++ labs/lab5/8.py
@@ -1,7 +1,7 @@
 import random
-user_inp = input('Выберите: Камень  Ножницы  Бумага  Ящерица  Спок (1-5)')
+user_inp = input('Выберите: Камень  Ножницы  Бумага  Ящерица  Спок (1-5): ')
 enemy_inp = random.randint(1,5)
-who_beats_who = {1:{2,4}, 2:{3,4}, 3:{1,5}, 4:{3,5}, 5:{1,2}}
+who_beats_who = {'1':{2,4}, '2':{3,4}, '3':{1,5}, '4':{3,5}, '5':{1,2}}
 if enemy_inp in who_beats_who[user_inp]:
     print('Вы победили!')
 else:
