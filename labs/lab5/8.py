60dbf9ad11286acc7ad8bf1e9a7d1ba4f152dd84
jarofsocks <stepankazuta@gmail.com>
2025-12-15 18:16:21 +1000

    .

--- labs/lab5/8.py
+++ labs/lab5/8.py
@@ -4,5 +4,7 @@ enemy_inp = random.randint(1,5)
 who_beats_who = {'1':{2,4}, '2':{3,4}, '3':{1,5}, '4':{3,5}, '5':{1,2}}
 if enemy_inp in who_beats_who[user_inp]:
     print('Вы победили!')
+elif user_inp == enemy_inp:
+    print('ничья!')
 else:
     print('Вы проиграли!')
